d31c6819b92d1f3bee010e2104466d3a350b8b5b
Nina Spinelli <ninafspin@gmail.com>
2022-02-18 01:36:16 -0300

    Logging and Artillery

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import compression from "compression"
 import core from "os"
+//import cluster from "cluster"
 import createLogger from "./utils.js"
 
 const app = express()
@@ -10,10 +11,6 @@ const logger = createLogger(process.env.NODE_ENV)
 
 //APP.USE
 app.use(compression())
-// app.use((req, res, next) => {
-//     logger.log("info", `${req.method} at ${req.path}`)
-//     next()
-// })
 
 //APP.GET
 app.get("/", (req, res) => {
@@ -21,10 +18,11 @@ app.get("/", (req, res) => {
 })
 app.get("/info", (req, res) => {
     logger.info("Info")
-    res.send({
+    const info = {
         status: "success",
         payload: {
             args: process.argv,
+            os: process.platform,
             nodeVersion: process.version,
             memoryUsage: process.memoryUsage(),
             execPath: process.execPath,
@@ -32,7 +30,9 @@ app.get("/info", (req, res) => {
             projectFolder: process.cwd(),
             cores: core.cpus().length
         }
-    })
+    }
+    //console.log(info);
+    res.send(info)
 })
 app.get("/error", (req, res) => {
     logger.error("An error has been detected")
--- src/utils.js
+++ src/utils.js
@@ -2,28 +2,28 @@ import winston from "winston"
 const {combine, timestamp, json} = winston.format
 
 const createLogger = (env) => {
-    // if (env === "PROD") {
+    if (env === "prod") {
         return winston.createLogger({
             format: combine(
                 timestamp(),
                 json()
             ),
             transports: [
-                new winston.transports.File({filename: "./logs/warn.log", level: "warn"}),
+                new winston.transports.File({filename: "./logs/warn.log", level: "warning"}),
                 new winston.transports.File({filename: "./logs/errors.log", level: "error"})
             ]
         })
-    // } else {
-    //     return winston.createLogger({
-    //         format: combine(
-    //             timestamp(),
-    //             json()
-    //         ),
-    //         transports: [
-    //             new winston.transports.Console({level: "all"})
-    //         ]
-    //     })
-    // }
+    } else {
+        return winston.createLogger({
+            format: combine(
+                timestamp(),
+                json()
+            ),
+            transports: [
+                new winston.transports.Console({level: "all"})
+            ]
+        })
+    }
 }
 
 export default createLogger
